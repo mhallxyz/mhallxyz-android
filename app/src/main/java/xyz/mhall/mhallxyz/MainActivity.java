c0bd8eb6ed8090dbf49b72f97b3e65713c5e3742
Mathew Hall <m@mhall.xyz>
2018-05-12 18:21:28 +0100

    First Commit

---
+++ app/src/main/java/xyz/mhall/mhallxyz/MainActivity.java
@@ -0,0 +1,25 @@
+package xyz.mhall.mhallxyz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = new WebView(this);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://mhall.xyz");
+        setContentView(myWebView);
+
+
+        Toast.makeText(getApplicationContext(), "It Works!!!!!", 10000).show();
+    }
+}
